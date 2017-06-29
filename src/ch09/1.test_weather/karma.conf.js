4c905bc5d7d06ddb736ed0aa59805fcf1ef1e422
Gim YoungPhil <youngphil.gim@gmail.com>
2017-06-29 13:47:39 +0900

    add 9.4

---
+++ src/ch09/1.test_weather/karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome', 'Firefox'],
+    frameworks: ['jasmine'],
+    reporters: ['dots'],
+    singleRun: true,
+    files: [
+      "node_modules/typescript/lib/typescript.js",
+      "node_modules/reflect-metadata/Reflect.js",
+      "node_modules/systemjs/dist/system.src.js",
+      "node_modules/zone.js/dist/zone.js",
+      "node_modules/zone.js/dist/async-test.js",
+      "node_modules/zone.js/dist/fake-async-test.js",
+      "node_modules/zone.js/dist/long-stack-trace-zone.js",
+      "node_modules/zone.js/dist/proxy.js",
+      "node_modules/zone.js/dist/sync-test.js",
+      "node_modules/zone.js/dist/jasmine-patch.js",
+
+      { pattern: 'karma-systemjs.config.js', included: true, watched: false },
+      { pattern: 'karma-test-runner.js', included: true, watched: false },
+
+      { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false },
+      { pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false },
+      { pattern: 'app/**/*.ts', included: false, watched: true },
+    ],
+    proxies: {
+      '/app/': '/base/app'
+    },
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher'
+    ]
+  })
+};
